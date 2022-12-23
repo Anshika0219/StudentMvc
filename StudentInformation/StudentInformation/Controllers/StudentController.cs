7039d297b88f84f3c82c408272b92b506635b4b1
Anshika0219 <anshikaa@moback.com>
2022-12-23 17:44:24 +0530

    Mvc Demo Project

---
+++ StudentInformation/StudentInformation/Controllers/StudentController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentInformation.Data;
+using StudentInformation.Models;
+
+namespace StudentInformation.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly InfoDbContext _infoDbContext;
+
+        public StudentController(InfoDbContext infoDbContext)
+        {
+            _infoDbContext = infoDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<StudentInfo> objStudentList = _infoDbContext.studentInfos.ToList();
+            return View(objStudentList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(StudentInfo student)
+        {
+            if (ModelState.IsValid)
+            {
+                _infoDbContext.studentInfos.Add(student);
+                _infoDbContext.SaveChanges();
+                TempData["success"] = "Category created successfully";
+            }
+            return View(student);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var StudentId = _infoDbContext.studentInfos.Find(id);
+            if (StudentId == null)
+            {
+                return NotFound();
+            }
+            return View(StudentId);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(StudentInfo student)
+        {
+            if (ModelState.IsValid)
+            {
+                _infoDbContext.studentInfos.Update(student);
+                    _infoDbContext.SaveChanges();
+                    TempData["success"] = "studentInfo updated successfully";
+                    return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+        public IActionResult Delete(int? id)
+        {
+            var studentId = _infoDbContext.studentInfos.Find(id);
+            if (studentId == null)
+            {
+                return NotFound();
+            }
+            return View(studentId);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(StudentInfo student)
+        {
+                _infoDbContext.studentInfos.Remove(student);
+                _infoDbContext.SaveChanges();
+                TempData["success"] = "Student deleted successfully";
+                return RedirectToAction("Index");
+          }
+       }
+    }
+
---
+++ StudentInformation/StudentInformation/Data/InfoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentInformation.Models;
+
+namespace StudentInformation.Data
+{
+    public class InfoDbContext : DbContext
+    {
+        public InfoDbContext(DbContextOptions<InfoDbContext> options) : base(options)
+        { 
+        }
+        public DbSet<StudentInfo> studentInfos { get; set; }
+    }
+}
